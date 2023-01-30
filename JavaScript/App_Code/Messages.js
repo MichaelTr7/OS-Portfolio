da5348fc5eecdfa9cf18e0c054cb2e3658a41436
MichaelTr7 <67344799+MichaelTr7@users.noreply.github.com>
2023-01-30 17:28:58 -0500

    Updated animations.

--- JavaScript/App_Code/Messages.js
+++ JavaScript/App_Code/Messages.js
@@ -3,7 +3,7 @@ function Setup_Messages_App(){
   console.log("Setup Messages App:");
   let Pinned_Chats = document.getElementsByClassName('Pinned_Chats');
   for(Index=0; Index < Pinned_Chats.length; Index++){
-    Pinned_Chats[Index].addEventListener('click',Pinned_Chats_Clicked);
+    // Pinned_Chats[Index].addEventListener('click',Pinned_Chats_Clicked);
   }
   
   
--- JavaScript/App_Code/Projects.js
+++ JavaScript/App_Code/Projects.js
@@ -1,9 +1,65 @@
 
 function Setup_Projects_App(){
   console.log("Setup Projects App:");
+  let Project_Selector_Buttons = document.getElementsByClassName('Project_Selector_Buttons');
+  for(let Button of Project_Selector_Buttons){
+    Button.addEventListener('mousedown',Filter_Projects);
+  }
+  
+
+}
+
+function Filter_Projects(){
+  this.classList.remove('Squeeze_Animation');
+  void this.offsetWidth;
+  this.classList.add('Squeeze_Animation');
+  
+  let Project_Selector_Buttons = document.getElementsByClassName('Project_Selector_Buttons');
+  for(let Button of Project_Selector_Buttons){
+    Button.classList.remove('Filter_Indicator');
+  }
+  
+  this.classList.add('Filter_Indicator');
+  let Project_Type = String(this.innerHTML);
+  
+  
+  let Project_Sections = document.getElementsByClassName('Project_Sections');
+  
+  let Index = 0;
+  for(let Project of Project_Sections){
+    
+    // if(Index % 2 == 0){
+    //   Project.classList.remove('Pulse_Fade_Animation');
+    //   void Project.offsetWidth;
+    //   Project.classList.add('Pulse_Fade_Animation');
+    // }else{
+    //   Project.classList.remove('Pulse_Fade_Animation_2');
+    //   void Project.offsetWidth;
+    //   Project.classList.add('Pulse_Fade_Animation_2');
+    // }
+
+  Project.style.display = "none";
+  if(String(Project.dataset.type) == Project_Type){
+    Project.style.display = "block";
+  }
+  
+  if(Project_Type == 'All'){
+    Project.style.display = "block";
+  }
+    Index = Index + 1;
+  }  
+  
+  var Window_Bodies = document.getElementById('Window_Bodies');
+  Window_Bodies.scrollTop = 0;
+    
+  
   
   
   
   
   
 }
+
+
+
+
