862fe07d0f8de9135e0dfdbdff45838dd4d3e6db
MichaelTr7 <67344799+MichaelTr7@users.noreply.github.com>
2023-02-06 21:03:46 -0500

    Updated swipe actions messages window.

--- JavaScript/App_Code/Messages.js
+++ JavaScript/App_Code/Messages.js
@@ -5,9 +5,28 @@ function Setup_Messages_App(){
   for(Index=0; Index < Pinned_Chats.length; Index++){
     // Pinned_Chats[Index].addEventListener('click',Pinned_Chats_Clicked);
   }
-  
-  
-  
-  
-  
+  let Recent_Chat_Boxes = document.getElementsByClassName('Recent_Chat_Boxes');
+  for(let Chat_Row of Recent_Chat_Boxes){
+    Chat_Row.addEventListener('scroll',Chat_Sliding);
+  }
 }
+
+
+function Chat_Sliding(){
+  let Recent_Chat_Boxes = document.getElementsByClassName('Recent_Chat_Boxes');
+  let Skipped_Index = [].indexOf.call(Recent_Chat_Boxes, this);
+  for(Index = 0; Index < Recent_Chat_Boxes.length; Index++){
+    if(parseInt(Index) != parseInt(Skipped_Index)){
+      if(Recent_Chat_Boxes[Index].scrollLeft >= 130){
+        Recent_Chat_Boxes[Index].scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+}
+
+  
+  
